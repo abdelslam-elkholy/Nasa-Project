58556259fd2bec4e89041baf5c1ed94db0c031b2
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-28 17:16:40 +0200

    update

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 const app = require("./app");
+const { loadPlanets } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+const startServer = async () => {
+  await loadPlanets();
+  server.listen(PORT, () => console.log(`app Is working on port ${PORT}`));
+};
 
-server.listen(PORT, () => console.log(`app Is working on port ${PORT}`));
+startServer();

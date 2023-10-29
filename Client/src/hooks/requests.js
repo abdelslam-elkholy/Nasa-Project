14a3d09e6a89017f65425c3466955f7eabb38752
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-29 18:14:11 +0200

    update

--- Client/src/hooks/requests.js
+++ Client/src/hooks/requests.js
@@ -1,15 +1,17 @@
 const API_URL = "http://localhost:5000";
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const res = await fetch(`${API_URL}/planets`);
   return await res.json();
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const res = await fetch(`${API_URL}/launches`);
+  const data = await res.json();
+
+  return data.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
 async function httpSubmitLaunch(launch) {

0aa5574803a8c101f6e70694003ab1191ce3b15b
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-31 20:20:03 +0200

    update

--- Server/src/routes/launches/launches.controller.js
+++ Server/src/routes/launches/launches.controller.js
@@ -6,7 +6,22 @@ const httpGetAllLaunches = (req, res) => {
 
 const httpCreateLaunche = (req, res) => {
   const launch = req.body;
+  if (
+    !launch.launchDate ||
+    !launch.rocket ||
+    !launch.misions ||
+    !launch.destination
+  ) {
+    res.status(404).json({
+      error: "Invalid launch INPUTS",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    res.status(404).json({ error: "Invalid launch Date" });
+  }
 
   createLaunch(launch);
   return res.status(201).json(launch);

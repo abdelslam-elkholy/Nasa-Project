0f20041d80738cf8766a00a461071391d3e9afdc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-31 20:21:45 +0200

    update

--- Server/src/routes/launches/launches.router.js
+++ Server/src/routes/launches/launches.router.js
@@ -6,6 +6,6 @@ const {
   httpCreateLaunche,
 } = require("./launches.controller");
 
-launchesRouter.use("/").get(httpGetAllLaunches).post(httpCreateLaunche);
+launchesRouter.route("/").get(httpGetAllLaunches).post(httpCreateLaunche);
 
 module.exports = launchesRouter;

12300d0799dd21763ab907597a34914f0e204ee1
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-03 03:46:44 +0200

    update

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,5 +1,4 @@
 const http = require("http");
-
 const mongoose = require("mongoose");
 
 const app = require("./app");
@@ -9,16 +8,14 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL =
   "mongodb+srv://abdelslamel5oly:nwWU5GVf0IGdUjXQ@cluster0.cyupft2.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connection.on("open", () => console.log("Database connection Ready!"));
+mongoose.connection.once("open", () =>
+  console.log("Database connection Ready!")
+);
+mongoose.connection.on("error", (err) => console.log("Error: " + err));
 
 const server = http.createServer(app);
 const startServer = async () => {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndexes: true,
-    useUniFiedTopology: true,
-  });
+  await mongoose.connect(MONGO_URL);
 
   await loadPlanets();
   server.listen(PORT, () => console.log(`app Is working on port ${PORT}`));

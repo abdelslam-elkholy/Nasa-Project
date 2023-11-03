0211bade9011e1860ded3670d7a76a08ec69f953
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-03 03:40:07 +0200

    update

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,12 +1,23 @@
 const http = require("http");
 
-const { loadPlanets } = require("./models/planets.model");
+const mongoose = require("mongoose");
+
 const app = require("./app");
+const { loadPlanets } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URL =
+  "mongodb+srv://abdelslamel5oly:nwWU5GVf0IGdUjXQ@cluster0.cyupft2.mongodb.net/?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndexes: true,
+    useUniFiedTopology: true,
+  });
+
   await loadPlanets();
   server.listen(PORT, () => console.log(`app Is working on port ${PORT}`));
 };

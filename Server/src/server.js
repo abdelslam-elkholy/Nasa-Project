975e9aeb1ca33ae343159a14e84a9c3641f7da8b
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-03 03:41:53 +0200

    update

--- Server/src/server.js
+++ Server/src/server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL =
   "mongodb+srv://abdelslamel5oly:nwWU5GVf0IGdUjXQ@cluster0.cyupft2.mongodb.net/?retryWrites=true&w=majority";
 
+mongoose.connection.on("open", () => console.log("Database connection Ready!"));
+
 const server = http.createServer(app);
 const startServer = async () => {
   await mongoose.connect(MONGO_URL, {

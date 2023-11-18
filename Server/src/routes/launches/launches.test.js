da8cfc6817ad296bcb86955b43d48e06040a1ba0
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-18 21:18:08 +0200

    update

--- Server/src/routes/launches/launches.test.js
+++ Server/src/routes/launches/launches.test.js
@@ -3,14 +3,22 @@ const app = require("../../app");
 
 describe("Test Get /Launches", () => {
   test("It Should response with 200 success", async () => {
-    const response = await request(app).get("/launches");
-
-    expect(response.statusCode).toBe(200);
+    const response = await request(app).get("/launches").expect(200);
   });
 });
 
 describe("Test Post / Launches", () => {
-  test("It Should response with 200 success", () => {});
+  test("It Should response with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Kepler-186 f",
+        launchDate: "January , 4 , 2028 ",
+      })
+      .expect(201);
+  });
 
   test("It Should catch missing required", () => {});
   test("It Should catch missing invalid dates", () => {});

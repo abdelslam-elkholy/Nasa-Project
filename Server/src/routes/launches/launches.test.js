5ce525a541f52caac2b7614bc0f1699c8b2c17d1
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-18 18:30:52 +0200

    update

---
+++ Server/src/routes/launches/launches.test.js
@@ -0,0 +1,13 @@
+describe("Test Get /Launches", () => {
+  test("It Should response with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test Post / Launches", () => {
+  test("It Should response with 200 success", () => {});
+
+  test("It Should catch missing required", () => {});
+  test("It Should catch missing invalid dates", () => {});
+});
